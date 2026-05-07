247a5bc7cb274ce8db28884d053c73508c0a0fe7
cchaitrasidamani-max <cchaitrasidamani@gmail.com>
2026-05-07 20:37:45 +0530

    schema.sql changed

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,8 @@
 -- ================================
 -- DATABASE
 -- ================================
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};;
-
-CREATE USER IF NOT EXISTS ${MYSQL_USER}@localhost IDENTIFIED BY '${MYSQL_PASSWORD}';
-
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO ${MYSQL_USER}@localhost;
-FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS smsdb;
+USE smsdb;
 
 -- ================================
 -- ROLES TABLE
